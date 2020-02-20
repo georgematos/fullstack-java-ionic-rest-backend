0c77d8f6d16c1391525afeeec57b54a80f6f13f7
George Matos <georgemattos@gmail.com>
2020-02-20 17:06:26 -0300

    criando profile de desenvolvimento

---
+++ src/main/java/com/geocode/fullstackproject/restbackend/config/DevConfig.java
@@ -0,0 +1,35 @@
+package com.geocode.fullstackproject.restbackend.config;
+
+import com.geocode.fullstackproject.restbackend.service.DBService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * DevConfig
+ */
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+  @Autowired
+  private DBService dbService;
+
+  @Value("${spring.jpa.hibernate.ddl-auto}")
+  private String strategy;
+
+  @Bean
+  public boolean instantiateDatabase() throws Exception {
+
+    if (!"create".equals(strategy)) {
+      return false;
+    }
+
+    dbService.instantiateTestDatabase();
+    return true;
+  }
+
+}
