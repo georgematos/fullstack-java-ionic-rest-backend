0b8db347e72f06245e508c2ed60da6b05f3d0e6c
George Matos <georgemattos@gmail.com>
2020-03-03 17:54:04 -0300

    salvando arquivo no S3 da Amazon

--- src/main/java/com/geocode/fullstackproject/restbackend/BackendRestApplication.java
+++ src/main/java/com/geocode/fullstackproject/restbackend/BackendRestApplication.java
@@ -1,13 +1,24 @@
 package com.geocode.fullstackproject.restbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendRestApplication {
+public class BackendRestApplication implements CommandLineRunner {
+
+	// @Autowired
+	// private S3Service s3service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendRestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// s3service.uploadFile("/home/george/Imagens/MinhasFotos/my_avatar.png");
+
 	}
 
 }
