3fb3f6b499737efb79d8a90745f214406560ef74
George Matos <georgemattos@gmail.com>
2020-04-20 05:35:02 -0300

    correcao do envio do header location para o cliente ao salvar um pedido

--- src/main/java/com/geocode/fullstackproject/restbackend/filters/HeaderExposureFilter.java
+++ src/main/java/com/geocode/fullstackproject/restbackend/filters/HeaderExposureFilter.java
@@ -22,7 +22,7 @@ public class HeaderExposureFilter implements Filter {
       throws IOException, ServletException {
 
     HttpServletResponse res = (HttpServletResponse) response;
-    res.addHeader("access-control-headers", "location");
+    res.addHeader("access-control-expose-headers", "location");
     chain.doFilter(request, response);
 
   }
